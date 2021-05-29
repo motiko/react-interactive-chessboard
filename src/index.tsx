ac88b269dd4a6231a90bf74fca31a3fbc3765f21
moti <moti.kor@gmail.com>
2021-05-29 22:01:26 +0200

    use-gesture

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useRef, RefObject } from 'react'
 import styles from './styles.module.css'
 import Piece from './Piece'
+import { useDrag } from '@use-gesture/react'
 interface Props {
   initialFen: string
 }
@@ -22,18 +23,84 @@ function fen2array(fen: string): Array<string> {
   return result
 }
 
+// function getMousePosition(evt) {
+//   var CTM = svg.getScreenCTM()
+//   return {
+//     x: (evt.clientX - CTM.e) / CTM.a,
+//     y: (evt.clientY - CTM.f) / CTM.d
+//   }
+// }
+
 export const ChessBoard = ({ initialFen }: Props) => {
   console.log(initialFen)
   const fenPieces = initialFen.split(' ')[0]
   const boardArr = fen2array(fenPieces)
   console.log(boardArr)
+  const svgRef = useRef<any>()
+  const bind = useDrag(({ down, movement: [mx, my] }) => {
+    if (down && svgRef?.current) {
+      console.log({ x: down ? mx : 0, y: down ? my : 0 })
+
+      const svgNode: any = svgRef.current
+      if (down) {
+        var CTM = svgNode.getScreenCTM()
+        const { x, y } = {
+          x: (mx - CTM.e) / CTM.a,
+          y: (my - CTM.f) / CTM.d
+        }
+        svgNode.setAttributeNS(
+          null,
+          'transform',
+          `scale(0.02) translate(${x} ${y}) `
+        )
+        // svgNode.setAttribute('x', `2`)
+        // svgNode.setAttribute('y', `2`)
+      }
+    }
+  })
   return (
-    <div className={styles.wrap}>
-      <div className={styles.board}>
-        {boardArr.map((pieceChar, i) => (
-          <Piece pieceChar={pieceChar} key={i} />
-        ))}
-      </div>
-    </div>
+    <svg viewBox='0 0 8 8' shapeRendering='crispEdges'>
+      <g id='board_svg__f'>
+        <g id='board_svg__e'>
+          <g id='board_svg__d'>
+            <g id='board_svg__c'>
+              <path fill='#f0d9b5' id='board_svg__a' d='M0 0h1v1H0z' />
+              <use x={1} y={1} href='#board_svg__a' xlinkHref='#a' />
+              <path fill='#b58863' id='board_svg__b' d='M0 1h1v1H0z' />
+              <use x={1} y={-1} href='#board_svg__b' xlinkHref='#b' />
+            </g>
+            <use x={2} href='#board_svg__c' xlinkHref='#c' />
+          </g>
+          <use x={4} href='#board_svg__d' xlinkHref='#d' />
+        </g>
+        <use y={2} href='#board_svg__e' xlinkHref='#e' />
+      </g>
+      <use y={4} href='#board_svg__f' xlinkHref='#f' />
+
+      <svg shapeRendering='crispEdges' x='0' y='0'>
+        <g
+          fill='none'
+          fillRule='evenodd'
+          stroke='#000'
+          strokeWidth={1.5}
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          transform='scale(0.02)'
+          ref={svgRef}
+          {...bind()}
+        >
+          <g fill='#000' strokeLinecap='butt'>
+            <path d='M9 36c3.39-.97 10.11.43 13.5-2 3.39 2.43 10.11 1.03 13.5 2 0 0 1.65.54 3 2-.68.97-1.65.99-3 .5-3.39-.97-10.11.46-13.5-1-3.39 1.46-10.11.03-13.5 1-1.35.49-2.32.47-3-.5 1.35-1.46 3-2 3-2z' />
+            <path d='M15 32c2.5 2.5 12.5 2.5 15 0 .5-1.5 0-2 0-2 0-2.5-2.5-4-2.5-4 5.5-1.5 6-11.5-5-15.5-11 4-10.5 14-5 15.5 0 0-2.5 1.5-2.5 4 0 0-.5.5 0 2z' />
+            <path d='M25 8a2.5 2.5 0 11-5 0 2.5 2.5 0 115 0z' />
+          </g>
+          <path
+            d='M17.5 26h10M15 30h15m-7.5-14.5v5M20 18h5'
+            stroke='#fff'
+            strokeLinejoin='miter'
+          />
+        </g>
+      </svg>
+    </svg>
   )
 }
--- src/pieces/white/P.tsx
+++ src/pieces/white/P.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import styles from './styles.module.css'
+import { useDrag } from '@use-gesture/react'
 
 function SvgP(props: any) {
+  const svgRef = useRef()
+  const bind = useDrag(({ down, movement: [mx, my] }) => {
+    // console.log({ x: down ? mx : 0, y: down ? my : 0 })
+    if (down && svgRef?.current) {
+      console.log({ x: down ? mx : 0, y: down ? my : 0 })
+      ;(
+        svgRef.current as any
+      ).style.transform = `translateX(${mx}), translateY(${my});`
+    }
+  })
   return (
-    <svg {...props} viewBox='0 0 40 40'>
+    <svg {...props} viewBox='0 0 40 40' ref={svgRef}>
       <path
+        className={styles.draggable}
+        {...bind()}
+        transform='translate(20,20)'
         d='M22.5 9c-2.21 0-4 1.79-4 4 0 .89.29 1.71.78 2.38C17.33 16.5 16 18.59 16 21c0 2.03.94 3.84 2.41 5.03-3 1.06-7.41 5.55-7.41 13.47h23c0-7.92-4.41-12.41-7.41-13.47 1.47-1.19 2.41-3 2.41-5.03 0-2.41-1.33-4.5-3.28-5.62.49-.67.78-1.49.78-2.38 0-2.21-1.79-4-4-4z'
         fill='#fff'
         stroke='#000'
